58facbcf49a456590b883da8db8182ec9b5e90b4
Qadri2012 <muhammadnqadri20@gmail.com>
2026-05-18 22:43:14 +0800

    setup github pages

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,15 +3,15 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-
+  base: "/NAMA-REPO/",
+  build: {
+    outDir: "docs",
+  },
   test: {
     environment: "jsdom",
     setupFiles: "./src/setupTests.js",
     globals: true,
   },
 });
-
-ihh
