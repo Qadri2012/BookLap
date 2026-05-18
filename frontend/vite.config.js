4ca0506b824dc4517f123a44641a6a7cf409048e
Qadri2012 <muhammadnqadri20@gmail.com>
2026-05-18 22:50:01 +0800

    setup github pages

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -5,10 +5,14 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  base: "/NAMA-REPO/",
+
+  base: "/BookLap/",
+
   build: {
-    outDir: "docs",
+    outDir: "../docs",
+    emptyOutDir: true,
   },
+
   test: {
     environment: "jsdom",
     setupFiles: "./src/setupTests.js",
