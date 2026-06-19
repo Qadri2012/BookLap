0617f9cf43fa1a10ace5e5bd020ab1669b2efb38
Qadri2012 <muhammadnqadri20@gmail.com>
2026-06-19 14:53:10 +0800

    commit3

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -21,10 +21,17 @@ const refreshCookieOptions = {
 
 // NEW: transporter email untuk OTP admin
 const mailTransporter = nodemailer.createTransport({
-  service: "gmail",
+  host: "smtp.gmail.com",
+  port: 587,
+  secure: false,
+
   auth: {
     user: process.env.SMTP_USER,
-    pass: process.env.SMTP_PASS, // Gmail App Password
+    pass: process.env.SMTP_PASS,
+  },
+
+  tls: {
+    rejectUnauthorized: false,
   },
 });
 
